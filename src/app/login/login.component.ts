159c740e6d50206bda1ca566840461478f44ce18
inbarajan <rajaninba6@gmail.com>
2023-07-14 07:36:24 +0530

    updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     public fb : FormBuilder,
-    private modalService: NgbModal,
+    public modalService: NgbModal,
     public activeModal: NgbActiveModal,
     private toastr: ToastrService,
     public service : AuthService,
